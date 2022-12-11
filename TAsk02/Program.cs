2a05830028b152b73301a1056098801e2e821404
Maxsot <sotnikov.maxim3007@gmail.com>
2022-12-10 22:12:27 -0800

    Task02

---
+++ TAsk02/Program.cs
@@ -0,0 +1,47 @@
+﻿// Задача 1: Напишите программу замены элементов массива: 
+// положительные элементы замените 
+// на соответствующие отрицательные, и наоборот.
+// [-4, -8, 8, 2] -> [4, 8, -8, -2] 
+int[] GenerateArray(int Length, int MinValue, int MaxValue)
+{
+    int[] numbers = new int[Length];
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        numbers[i] = new Random().Next(MinValue, MaxValue);
+    }
+    return numbers;
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.WriteLine($"{array[i]}\t");
+    }
+}
+
+void ChangeNummbers(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        int number = array[i];
+        if (number < 0)
+        {
+            number = number * (-1);
+            Console.Write($"{number}\t");
+        }
+        else
+        {
+            Console.Write($"{array[i]}\t");
+        }
+
+    }
+}
+
+int length = 5;
+int minRange = -100;
+int maxRange = 800;
+
+int[] arr = GenerateArray(length, minRange, maxRange);
+PrintArray(arr);
+ChangeNummbers(arr);
